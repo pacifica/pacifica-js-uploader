f5dad36d9d94fb41da4a68f0ab27ff6e16ba78fd
David Brown <dmlb2000@gmail.com>
2017-10-07 23:15:14 -0700

    try getting 100% coverage

--- test/test_FileObj.js
+++ test/test_FileObj.js
@@ -15,6 +15,8 @@ exports.testFileObj = function(test) {
     for(i = 0; i < attrs.length; i++) {
         test.ok(test_obj.hasOwnProperty(attrs[i]), 'FileObj should have '+attrs[i]+' property.');
     }
+    test_obj = new FileObj('{"name": "blarg"}');
+    test.equal(test_obj.name, 'blarg', 'passing json to fileobj sets the properties.');
     test.done();
 };
 
