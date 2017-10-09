3a6a49bbddb9548802a9b9f4615a133a729531be
David Brown <dmlb2000@gmail.com>
2017-10-09 09:04:00 -0700

    add isValid method

--- test/test_MetaData.js
+++ test/test_MetaData.js
@@ -11,9 +11,10 @@ exports.testMetaData = function (test) {
 exports.testMetaDataID = function (test) {
     'use strict';
     var MetaData = require('../lib/MetaData');
-    var test_obj = new MetaData('[{"metaID": "blarg"}]');
+    var test_obj = new MetaData('[{"metaID": "blarg", "value": "blah"}]');
     test.equal(test_obj.byMetaID('blarg').metaID, 'blarg', 'finding metaobj by metaID works.');
     test.equal(test_obj.byMetaID('blah'), undefined, 'finding metaobj by unknown metaID works.');
+    test.ok(test_obj.isValid(), 'value is set so the md_obj is valid.');
     test.done();
 };
 
