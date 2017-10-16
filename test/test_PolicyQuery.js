00053e83c32c0536a9fa9e1b9422831b204a3950
David Brown <dmlb2000@gmail.com>
2017-10-15 21:05:21 -0700

    this make the testing work

---
+++ test/test_PolicyQuery.js
@@ -0,0 +1,31 @@
+exports.testPolicyQuery = function (test) {
+    'use strict';
+    test.expect(3);
+    var PolicyQuery = require('../lib/PolicyQuery');
+    var pq_data = {
+        columns: ['_id'],
+        from: 'instruments',
+        where: { _id: 54 }
+    }
+    var test_obj = new PolicyQuery('dmlb2001', { pq_data: pq_data });
+    test_obj.setUser(function() {
+        test.equal(test_obj.pq_data.user, 10, 'Make sure user is now set to number not name.');
+        test.equal(test_obj.pq_data.from, 'instruments', 'Make sure the original query was not changed.');
+        test_obj.get_results(function(test_obj, results) {
+            test.equal(results[0]['_id'], 54, 'instrument ID should be 54');
+        });
+    });
+    setTimeout(function () {
+        test.done();
+    }, 2000);
+};
+exports.testPolicyQueryBad = function (test) {
+    'use strict';
+    test.expect(2);
+    var PolicyQuery = require('../lib/PolicyQuery');
+    var test_obj = PolicyQuery();
+    test.equal(test_obj, undefined, 'This is an invalid object.');
+    test_obj = PolicyQuery('dmlb2001', {});
+    test.equal(test_obj, undefined, 'No policy query data can not query on nothing.');
+    test.done();
+};
---
+++ test/test_Utils.js
@@ -0,0 +1,36 @@
+exports.testUtilsServerURL = function (test) {
+    'use strict';
+    var Utils = require('../lib/Utils');
+    var test_obj = {};
+    test_obj._addr = '127.0.0.1';
+    process.env['METADATA_PATH'] = '/md_path';
+    Utils.ServerURL(
+        test_obj,
+        [
+            ['port', '8121'],
+            ['addr', '1.2.3.4']
+        ],
+        'METADATA'
+    );
+    [
+        '_addr',
+        '_port',
+        '_path',
+        '_proto',
+        '_url'
+    ].forEach(function (attr_name) {
+        test.ok(test_obj.hasOwnProperty(attr_name), 'Test obj should have ' + attr_name + ' property.');
+    });
+    test_obj = {};
+    Utils.ServerURL(
+        test_obj,
+        [
+            ['port', '8121'],
+            ['url', 'https://1.2.3.4:8121/blarg']
+        ],
+        'METADATA'
+    );
+    test.equal(test_obj._url, 'https://1.2.3.4:8121/blarg');
+    test.done();
+};
+
