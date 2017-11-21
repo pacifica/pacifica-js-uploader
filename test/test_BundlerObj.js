7dafbe4360b2fcdf489f59f8b1dc80b149b575db
David Brown <dmlb2000@gmail.com>
2017-11-21 14:53:07 -0800

    Add bundler code (#4)
    * add new metaObj and fix jslint issues
    
    * add jslint to the project
    
    * add metadata object
    
    * try adding eslintrc file
    
    * we needed to fix the tasks
    
    * add isValid method
    
    * add policy query data
    
    * fix jshint
    
    * this make the testing work
    
    * try more linting
    
    * got it to a minimum
    
    * try to get jslint working happily
    
    * try fixing this
    
    * eslint doesn't error when it fixes
    
    * do not check white space with jslint that's eslints job
    
    * add travis bits to build up env
    
    * add services to travis
    
    * try making python work
    
    * add the new config files
    
    * remove not needed file
    
    * seems like this will still return a valid empty PolicyQuery object
    
    * add bundler deps
    
    * add bundling code
    
    * more lint issues
    
    * add one more file
    
    * do better things with multiple things in a tar
    
    * this shouldn't be there

---
+++ test/test_BundlerObj.js
@@ -0,0 +1,61 @@
+exports.testBundlerObj = function (test) {
+    'use strict';
+    var BundlerObj = require('../lib/BundlerObj'),
+        fs = require('fs'),
+        tar = require('tar-stream'),
+        tempfile = require('tempfile'),
+        temptar = tempfile('.tar'),
+        MetaData = require('../lib/MetaData'),
+        Promise = require('promise'),
+        md_obj = new MetaData(),
+        file_promises = [];
+    test.expect(5);
+    [
+        ['README.md', 'data/README.md'],
+        ['package.json', 'blarg/package.json']
+    ].forEach(function (part) {
+        file_promises.push(new Promise( function (resolve, reject) {
+            var filepath = part[0], archpath = part[1];
+            fs.stat(filepath, function (err, stats) {
+                if(err) { reject(err); }
+                resolve({
+                    'tarEntryArgs': {
+                        'name': archpath,
+                        'size': stats.size,
+                        'mtime': stats.mtime
+                    },
+                    'readStream': fs.createReadStream(filepath)
+                });
+            });
+        }));
+    });
+    Promise.all(file_promises).then( function (file_data) {
+        var mytar = fs.createWriteStream(temptar),
+            test_obj = new BundlerObj(md_obj, file_data, 'sha1');
+        test_obj.stream(mytar, function () {
+            mytar.on('finish', function () {
+                var extract = tar.extract(),
+                    names = ['data/README.md', 'blarg/package.json', 'metadata.txt'];
+                test.ok(true, 'Got to end the tarfile.');
+                extract.on('entry', function(header, stream, next) {
+                    test.equal(header.name, names[0], 'names for header matches.');
+                    names.shift();
+                    stream.on('end', function() {
+                        next();
+                    });
+                    stream.resume();
+                });
+                extract.on('finish', function() {
+                    test.ok(true, 'Finished extracting the tar.');
+                });
+                fs.createReadStream(temptar).pipe(extract);
+            });
+            mytar.flush();
+            mytar.end();
+        });
+    });
+    setTimeout(function () {
+        test.done();
+    }, 3000);
+};
+
