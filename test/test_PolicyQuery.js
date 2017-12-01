cd97b7558f215b980fcee2f422a7a8c1d4197c85
David Brown <dmlb2000@gmail.com>
2017-12-01 14:30:00 -0800

    need to get some ingest checks (#9)

--- test/test_PolicyQuery.js
+++ test/test_PolicyQuery.js
@@ -17,6 +17,34 @@ exports.testPolicyQuery = function (test) {
         test.done();
     }, 2000);
 };
+exports.testPolicyQueryMetaData = function (test) {
+    'use strict';
+    test.expect(2);
+    var PolicyQuery = require('../lib/PolicyQuery'),
+        MetaData = require('../lib/MetaData'),
+        MetaObj = require('../lib/MetaObj'),
+        md_obj = new MetaData(),
+        test_obj = new PolicyQuery('dmlb2001', {pq_data:{}});
+    md_obj.push(new MetaObj({
+        'destinationTable': 'Transactions.submitter',
+        'value': 10
+    }));
+    md_obj.push(new MetaObj({
+        'destinationTable': 'Transactions.proposal',
+        'value': '1234a'
+    }));
+    md_obj.push(new MetaObj({
+        'destinationTable': 'Transactions.instrument',
+        'value': 54
+    }));
+    test_obj.valid_metadata(md_obj).then( function (results) {
+        test.ok(results.hasOwnProperty('status'), 'results has status attribute');
+        test.equal(results.status, 'success', 'status is successful');
+    });
+    setTimeout(function () {
+        test.done();
+    }, 2000);
+};
 exports.testPolicyQueryBad = function (test) {
     'use strict';
     test.expect(2);
